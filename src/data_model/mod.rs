6b18bc0025d99cd84c13646c851cb38aed20a191
SIMRAN MAKHIJA <simran.makhija2@gmail.com>
2024-02-27 23:06:13 -0500

    remove unused import

--- src/data_model/mod.rs
+++ src/data_model/mod.rs
@@ -2,6 +2,6 @@ pub mod namespace_data;
 pub mod table_data;
 pub mod operator_statistics;
 
-pub use namespace_data::NamespaceData;
-pub use table_data::TableData;
-pub use operator_statistics::OperatorStatistics;
+// pub use namespace_data::NamespaceData;
+// pub use table_data::TableData;
+// pub use operator_statistics::OperatorStatistics;
