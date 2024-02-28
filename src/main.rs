6e81d57c078c6459b0b34f4c492014dc13aa4980
Aditya-06 <adityaajmera060401@gmail.com>
2024-02-28 01:13:08 -0500

    feat/skeletal routes implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+mod dto;
+mod handlers;
+
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app = routes::root::routes();
+    axum::serve(listener, app).await.unwrap();
 }
