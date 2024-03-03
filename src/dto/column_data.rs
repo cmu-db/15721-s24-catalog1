24a64ebf77d090a9fa73b30c5eeefd2c4ae3b17e
SIMRAN MAKHIJA <simran.makhija2@gmail.com>
2024-03-02 20:56:52 -0500

    add ColumnData struct

---
+++ src/dto/column_data.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ColumnData{
+    pub aggregates: Value,
+    pub value_range: (i32, i32),
+    pub is_strong_key: bool,
+    pub is_weak_key: bool,
+    pub primary_key_col_name: String,
+}
