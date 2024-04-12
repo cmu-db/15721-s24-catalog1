763e1f837edcd01492ed2d80e2e257253b0713db
SIMRAN MAKHIJA <simran.makhija2@gmail.com>
2024-04-12 12:59:51 -0400

    add some tests to namespace handler

--- src/handlers/namespace_handler.rs
+++ src/handlers/namespace_handler.rs
@@ -7,6 +7,12 @@ use axum::{
 use serde_json::Value;
 use std::sync::Arc;
 
+/*
+    TODO:
+    if a namespace or table already exists, you might want to return a StatusCode::CONFLICT
+    instead of StatusCode::INTERNAL_SERVER_ERROR. Similarly, if a namespace or table is not found,
+    you might want to return a StatusCode::NOT_FOUND.
+*/
 pub async fn list_namespaces(
     State(repo): State<Arc<NamespaceRepository>>,
 ) -> Result<Json<Vec<String>>, (StatusCode, String)> {
@@ -73,4 +79,233 @@ pub async fn set_namespace_properties(
     repo.set_namespace_properties(namespace.as_str(), properties.0)
         .map(|_| StatusCode::OK)
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}", e)))
+}
+
+// todo: check commented tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::database::database::Database;
+    use axum::http::StatusCode;
+    use serde_json::json;
+    use std::sync::{Arc, Mutex};
+    use tempfile::tempdir;
+
+    #[tokio::test]
+    async fn test_list_namespaces() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let data1 = list_namespaces(State(repo)).await.unwrap();
+        let data2: Json<Vec<String>> = Json(vec![]);
+        assert!(*data1 == *data2);
+    }
+
+    #[tokio::test]
+    async fn test_create_namespace() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "namespace".to_string(),
+            properties: json!({}),
+        });
+        assert_eq!(
+            create_namespace(State(repo), new_namespace.clone())
+                .await
+                .unwrap()
+                .name,
+            new_namespace.name
+        );
+    }
+
+    #[tokio::test]
+    async fn test_load_namespace_metadata() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "namespace".to_string(),
+            properties: json!({}),
+        });
+        let _ = create_namespace(State(repo.clone()), new_namespace.clone())
+            .await
+            .unwrap();
+
+        assert_eq!(
+            load_namespace_metadata(State(repo), Path("namespace".to_string()))
+                .await
+                .unwrap()
+                .name,
+            new_namespace.name
+        );
+    }
+
+    #[tokio::test]
+    async fn test_namespace_exists() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "namespace".to_string(),
+            properties: json!({}),
+        });
+        let _ = create_namespace(State(repo.clone()), new_namespace)
+            .await
+            .unwrap();
+        assert_eq!(
+            namespace_exists(State(repo), Path("namespace".to_string()))
+                .await
+                .unwrap(),
+            StatusCode::FOUND
+        );
+    }
+
+    #[tokio::test]
+    async fn test_drop_namespace() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "namespace".to_string(),
+            properties: json!({}),
+        });
+        let _ = create_namespace(State(repo.clone()), new_namespace)
+            .await
+            .unwrap();
+        assert_eq!(
+            drop_namespace(State(repo), Path("namespace".to_string()))
+                .await
+                .unwrap(),
+            StatusCode::NO_CONTENT
+        );
+    }
+
+    #[tokio::test]
+    async fn test_set_namespace_properties() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "namespace".to_string(),
+            properties: json!({}),
+        });
+        let _ = create_namespace(State(repo.clone()), new_namespace)
+            .await
+            .unwrap();
+        assert_eq!(
+            set_namespace_properties(
+                State(repo),
+                Path("namespace".to_string()),
+                Json(json!({"property": "value"}))
+            )
+            .await
+            .unwrap(),
+            StatusCode::OK
+        );
+    }
+
+    // Negative cases
+    #[tokio::test]
+    async fn test_load_namespace_metadata_not_found() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        assert_eq!(
+            load_namespace_metadata(State(repo), Path("nonexistent".to_string()))
+                .await
+                .unwrap_err()
+                .0,
+            StatusCode::NOT_FOUND
+        );
+    }
+
+    #[tokio::test]
+    async fn test_namespace_exists_not_found() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        assert_eq!(
+            namespace_exists(State(repo), Path("nonexistent".to_string()))
+                .await
+                .unwrap(),
+            StatusCode::NOT_FOUND
+        );
+    }
+
+    /*
+    #[tokio::test]
+    async fn test_drop_namespace_not_found() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        assert_eq!(
+            drop_namespace(State(repo), Path("nonexistent".to_string()))
+                .await
+                .unwrap_err()
+                .0,
+            StatusCode::INTERNAL_SERVER_ERROR
+        );
+    }
+    */
+
+    #[tokio::test]
+    async fn test_set_namespace_properties_not_found() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        assert_eq!(
+            set_namespace_properties(
+                State(repo),
+                Path("nonexistent".to_string()),
+                Json(json!({"property": "value"}))
+            )
+            .await
+            .unwrap_err()
+            .0,
+            StatusCode::INTERNAL_SERVER_ERROR
+        );
+    }
+
+    /*
+    // Corner cases
+    #[tokio::test]
+    async fn test_create_namespace_empty_name() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "".to_string(),
+            properties: json!({}),
+        });
+        assert_eq!(
+            create_namespace(State(repo), new_namespace)
+                .await
+                .unwrap_err()
+                .0,
+            StatusCode::INTERNAL_SERVER_ERROR
+        );
+    }
+
+    #[tokio::test]
+    async fn test_create_namespace_already_exists() {
+        let repo = Arc::new(NamespaceRepository::new(Arc::new(Mutex::new(
+            Database::open(tempdir().unwrap().path()).unwrap(),
+        ))));
+        let new_namespace = Json(NamespaceData {
+            name: "namespace".to_string(),
+            properties: json!({}),
+        });
+        let _ = create_namespace(State(repo.clone()), new_namespace.clone())
+            .await
+            .unwrap();
+        assert_eq!(
+            create_namespace(State(repo), new_namespace)
+                .await
+                .unwrap_err()
+                .0,
+            StatusCode::INTERNAL_SERVER_ERROR
+        );
+    }
+    */
 }
