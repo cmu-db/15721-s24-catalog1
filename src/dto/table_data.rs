aa645256fcabe9cce601ae84102dc69db559b858
Aditya-06 <adityaajmera060401@gmail.com>
2024-05-01 17:38:10 -0400

    chore/update table indent structs

--- src/dto/table_data.rs
+++ src/dto/table_data.rs
@@ -2,6 +2,8 @@ use crate::dto::column_data::ColumnData;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+use super::namespace_data::NamespaceIdent;
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct TableData {
     pub name: String,
@@ -11,6 +13,35 @@ pub struct TableData {
     pub file_urls: Vec<String>,
     pub columns: Vec<ColumnData>,
 }
+
+/// TableIdent represents the identifier of a table in the catalog.
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TableIdent {
+    /// Namespace of the table.
+    pub namespace: NamespaceIdent,
+    /// Table name.
+    pub name: String,
+}
+
+impl TableIdent {
+    /// Create a new table identifier.
+    pub fn new(namespace: NamespaceIdent, name: String) -> Self {
+        Self { namespace, name }
+    }
+
+    /// Get the namespace of the table.
+    /// this returns the identifier of the namespace
+    pub fn namespace(&self) -> &NamespaceIdent {
+        &self.namespace
+    }
+
+    /// Get the name of the table.
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+}
+
 
 #[cfg(test)]
 mod tests {
--- src/structs/table.rs
+++ src/structs/table.rs
@@ -1,15 +1,10 @@
-use crate::table::Table;
 use crate::structs::error::{Error, ErrorKind, Result};
-use crate::structs::namespace::{Namespace, NamespaceIdent};
-use async_trait::async_trait;
-use serde_derive::{Deserialize, Serialize};
+use crate::dto::namespace_data::{NamespaceData, NamespaceIdent};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::mem::take;
 use std::ops::Deref;
-use typed_builder::TypedBuilder;
-use urlencoding::encode;
-use uuid::Uuid;
 
 
 /// TableIdent represents the identifier of a table in the catalog.
@@ -37,30 +32,4 @@ impl TableIdent {
     pub fn name(&self) -> &str {
         &self.name
     }
-
-    /// Try to create table identifier from an iterator of string.
-    pub fn from_strs(iter: impl IntoIterator<Item = impl ToString>) -> Result<Self> {
-        let mut vec: Vec<String> = iter.into_iter().map(|s| s.to_string()).collect();
-        let table_name = vec.pop().ok_or_else(|| {
-            Error::new(ErrorKind::DataInvalid, "Table identifier can't be empty!")
-        })?;
-        let namespace_ident = NamespaceIdent::from_vec(vec)?;
-
-        Ok(Self {
-            namespace: namespace_ident,
-            name: table_name,
-        })
-    }
-}
-
-/// TableCreation represents the creation of a table in the catalog.
-#[derive(Debug, TypedBuilder)]
-pub struct TableCreation {
-    /// The name of the table.
-    pub name: String,
-    /// The schema of the table.
-    pub schema: Schema,
-    /// The properties of the table.
-    #[builder(default)]
-    pub properties: HashMap<String, String>,
 }
