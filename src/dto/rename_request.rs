8a12a01a91aa91eec09a945cfa0390a662d319dd
SIMRAN MAKHIJA <simran.makhija2@gmail.com>
2024-05-02 02:15:20 -0400

    add test

--- src/dto/rename_request.rs
+++ src/dto/rename_request.rs
@@ -1,7 +1,7 @@
 use crate::dto::table_data::TableIdent;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct TableRenameRequest {
     pub source: TableIdent,
     pub destination: TableIdent,
